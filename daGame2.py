b1b704456b18ee499d3e4787bb8f2bad2f53dbd8
Optihiker <empropartners@gmail.com>
2017-03-29 14:25:28 -0700

    Stashing Changes

--- daGame2.py
+++ daGame2.py
@@ -99,10 +99,10 @@ class Healthpotion(Potion):
 class Suicidepotion(Potion):
     use = "KILLS!!!!!"
     def use():
-    if mainCharacter.Suicidepotion >1:
-        mainCharacter.Suicidepotion - 1
-    else:
-        print "Hacker!!!"
+        if mainCharacter.Suicidepotion >1:
+            mainCharacter.Suicidepotion - 1
+        else:
+            print "Hacker!!!"
 
 
 class Armor():
@@ -187,16 +187,14 @@ class Battle:
                     enemyNum = raw_input("What enemey(s) would you like to fight? Enter a number based on the info from above:\n")
 
                     try:
-                        enemyNum = int(inpt)
+                        enemyNum = int(enemyNum)
                     except:
                          pass
 
                 if inpt == "1":
                     mainCharacter.attack(list[enemyNum - 1])
                 elif inpt =="2":
-
                     mainCharacter.specialMove(list[enemyNum - 1])
-
                 elif inpt =="3":
                     mainCharacter.Inventory.Openbag()
 
